de1087c4e4885778400834e94b7b8dbcbdb83a0f
aoeuth <atoheusn@gmail.com>
2025-08-10 14:21:26 -0400

    refactor: improve code formatting and readability

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var peer = new Peer({debug: 3});
 var conn = null;
 
 
-peer.on('open', function(id) {
-	console.log('My peer ID is: ' + id);
-  });
+peer.on("open", function (id) {
+  console.log("My peer ID is: " + id);
+});
 
 // function canvasClear(){
 //     ctx.fillStyle = "rgba(222, 230, 246, 1)";
@@ -18,70 +18,70 @@ peer.on('open', function(id) {
 //     ctx.fillRect(0, 0, canvas.width, canvas.height);
 // }
 
-function drawText(){
-    // let canvasDrawX = 0;
-    // let canvasDrawY = 0;
+function drawText() {
+  // let canvasDrawX = 0;
+  // let canvasDrawY = 0;
 
-    // const XIncrement = 64;
-    // const YIncrement = 64;
+  // const XIncrement = 64;
+  // const YIncrement = 64;
 
-    // canvasClear();
+  // canvasClear();
 
-    // for (let i = 0; i < chat.length; i++){
-    //     ctx.drawImage(document.getElementById(chat[i]), canvasDrawX, canvasDrawY);
-    //     canvasDrawX += XIncrement;
-    //     if (canvasDrawX + XIncrement > canvas.width){
-    //         canvasDrawX = 0;
-    //         canvasDrawY += YIncrement;
-    //     }
-    // }
-    document.getElementById("chat").innerHTML = chat;
+  // for (let i = 0; i < chat.length; i++){
+  //     ctx.drawImage(document.getElementById(chat[i]), canvasDrawX, canvasDrawY);
+  //     canvasDrawX += XIncrement;
+  //     if (canvasDrawX + XIncrement > canvas.width){
+  //         canvasDrawX = 0;
+  //         canvasDrawY += YIncrement;
+  //     }
+  // }
+  document.getElementById("chat").innerHTML = chat;
 }
 
 // Register when a key is pressed and add it to the paragraphText
 document.body.onkeydown = function (key) {
-    if (key.key === "Backspace"){
-        chat = chat.slice(0, -1);
-    }
-    else{
-        chat += key.key;
-    }
-    sendKey(key.key);
-    drawText();
+  if (key.key === "Backspace") {
+    chat = chat.slice(0, -1);
+  } else {
+    chat += key.key;
+  }
+  sendKey(key.key);
+  drawText();
 };
 
 function receiveKey(key) {
-    if (letterImageMap.has(key)){
-        chat += key;
-    }
-    else if (key === "Backspace"){
-        chat = chat.slice(0, -1);
-    }
-    drawText();
+  if (letterImageMap.has(key)) {
+    chat += key;
+  } else if (key === "Backspace") {
+    chat = chat.slice(0, -1);
+  }
+  drawText();
 }
 
-function sendKey(key){
-    // Broadcast the key you pressed to all peers on the network
-    // for (let i = 0; i < conList.length; i++){
-    //     conList[i].send(key);
-    // }
+function sendKey(key) {
+  // Broadcast the key you pressed to all peers on the network
+  // for (let i = 0; i < conList.length; i++){
+  //     conList[i].send(key);
+  // }
+  if (conn && conn.open) {
     conn.send(key);
+  } else {
+    console.warn("Connection not open, key not sent");
+  }
 }
 
-function getSelfId(){
-    document.getElementById("IdBox").innerHTML = "Your id is: " + peer.id;
+function getSelfId() {
+  document.getElementById("IdBox").innerHTML = "Your id is: " + peer.id;
 }
 
-function connectButtonPressed(){
-    setTimeout(() => {
-        conn = peer.connect(document.getElementById("hostInput").value);
-    }, 1000);
-    setupConnection(conn);
-    console.log("Established connection with user: " + conn.peer);
+function connectButtonPressed() {
+  conn = peer.connect(document.getElementById("hostInput").value);
+  setupConnection(conn);
+  console.log("Established connection with user: " + conn.peer);
 }
 
-function changeDisplayToConnected(){
-    document.getElementById("IdBox").innerHTML = "Connected! :)";
+function changeDisplayToConnected() {
+  document.getElementById("IdBox").innerHTML = "Connected! :)";
 }
 
 function setupConnection(connection) {
@@ -97,22 +97,22 @@ function setupConnection(connection) {
   });
 }
 
-peer.on('connection', function(conn) {
-    setTimeout(() => {
-        setupConnection(conn);
-    }, 1000);
+peer.on("connection", function (conn) {
+  setTimeout(() => {
+    setupConnection(conn);
+  }, 1000);
 });
 
 function resizePage() {
 
-    // canvas.width = window.innerWidth * 0.9;
-    // canvas.height = window.innerHeight * 0.8;
+  // canvas.width = window.innerWidth * 0.9;
+  // canvas.height = window.innerHeight * 0.8;
 
-    // canvasClear();
+  // canvasClear();
 
-    drawText();
+  drawText();
 }
 
 window.onload = window.onresize = function () {
-    resizePage();
+  resizePage();
 };
